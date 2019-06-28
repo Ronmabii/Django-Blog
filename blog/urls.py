2194a78f4b99f824a26006c3cff32c41cff4f40e
ronmabii <ronmabii@gmail.com>
2019-06-27 22:29:35 -0400

    switched pages again

--- blog/urls.py
+++ blog/urls.py
@@ -3,14 +3,14 @@ from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView,
 from . import views
 
 urlpatterns = [
-	path('', views.front_page, name='blog-about'),
-    path('blog/', PostListView.as_view(), name='blog-home'),
+	path('', views.front_page, name='blog-about'), # The About page 
+    path('blog/', PostListView.as_view(), name='blog-posts'), # The good looking posts page
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('blog/', PostListView.as_view(), name='blog-home')
+    #path('blog/', PostListView.as_view(), name='blog-home') // This is probably extra but keeping as record
 ]
 
 #path('', PostListView.as_view(), name='blog-home') 	
